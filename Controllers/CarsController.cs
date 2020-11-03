7c5d80c9a9a92fa441d03b5d281087e35b45644f
Harpeprisme <j.abergel1@gmail.com>
2020-11-03 14:09:15 +0100

    Get Ok

---
+++ Controllers/CarsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CarManagement.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarsController : ControllerBase
+    {
+        private static readonly string[] CarTypes = new[]
+        {
+            "Polo", "Golf", "Passat", "Tiguan", "Caddy", "Touareg", "T-Roc", "Arteon", "Sharan", "T-cross"
+        };
+        private readonly ILogger<CarsController> _logger;
+
+        public CarsController(ILogger<CarsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Cars> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Cars
+            {
+                Id = rng.Next(0, 55),
+                Brand = "Volkswagen",
+                CarRegistrationDate = DateTime.Now.AddDays(index),
+                Type = CarTypes[rng.Next(CarTypes.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Models/Cars.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CarManagement
+{
+    public class Cars
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public DateTime CarRegistrationDate { get; set; }
+    }
+}
